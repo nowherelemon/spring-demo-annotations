8d45392fe0b0fb7fc6509242017d68ec0428ff35
nowherelemon <lemonnowhere@gmail.com>
2017-06-09 22:28:59 -0700

    Java Configuration Class

---
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -0,0 +1,23 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:sport.properties")
+//@ComponentScan("com.luv2code.springdemo")
+public class SportConfig {
+	//define bean for our sad fortuneSerivce
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	//define bean for swimCoach and inject dependency
+	@Bean
+	public Coach swimCoach() {
+		return new SwimCoach(sadFortuneService());
+	}
+}
